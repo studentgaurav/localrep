bd799fb00736d28380c5efdc86da6853138b4ea6
studentgaurav <shahoffical2@gmail.com>
2024-04-01 13:12:00 +0530

    add inital files

---
+++ pyhton.py
@@ -0,0 +1,11 @@
+print("hello world")
+
+b=str(input("enter your name:"))
+
+print("WELCOME:",b)
+
+C=str(input("what is your hobby?"))
+
+print("YOU ARE GOOD IN:",c,"?")
+print("answer in true or false")
+input("enter")
